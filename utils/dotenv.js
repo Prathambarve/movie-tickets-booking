2ac2e99dd2a994d8a60e844fd3cffc849c5f03a7
Oleksii Habiiev <admin@habiiev.wtf>
2020-05-25 20:41:21 +0300

    Update dotenv util
    Dotenv util will now strip env file from comments

--- utils/dotenv.js
+++ utils/dotenv.js
@@ -7,14 +7,21 @@ const ENV_REGEXP = /^.+=.+$/;
 
 // Function that loads .env file with a given path
 module.exports = async envPath => {
-  const fileContents = await fsp.readFile(envPath, 'utf-8');
-  fileContents.split('\n').forEach(line => {
-    line = line.trim();
-    if (line === '') return;
-    if (line.startsWith('#')) return;
-    if (!ENV_REGEXP.test(line)) throw 'invalid .env';
+  fs.access(envPath, fs.F_OK, async err => {
+    if (err) return;
 
-    const [name, value] = line.split('=');
-    process.env[name] = value;
+    const fileContents = await fsp.readFile(envPath, 'utf-8');
+    fileContents.split('\n').forEach(line => {
+      if (line === '') return;
+
+      // Strip all the comments
+      line.replace(/#.*$/, '');
+      line = line.trim();
+
+      if (!ENV_REGEXP.test(line)) throw 'invalid .env';
+
+      const [name, value] = line.split('=');
+      process.env[name] = value;
+    });
   });
 };
